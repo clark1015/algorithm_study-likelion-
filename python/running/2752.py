24e65dd68de814efdbffcc2dfb814055ccfb70eb
clark1015 <seok626898@gmail.com>
2021-10-28 10:20:33 +0900

    2nd code

--- python/running/2752.py
+++ python/running/2752.py
@@ -6,3 +6,11 @@ for i in range(len(num_list)):
 for i in num_list:
     print(i, end=' ')
 
+
+num_list = list(map(int, input().split()))
+result = []
+for i in range(3):
+    result.append(min(num_list))
+    num_list.remove(min(num_list))
+for i in result:
+    print(i, end = ' ')
